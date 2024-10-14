ba04c4847a131b86c54ed4ba99a63734c73ec37c
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2024-10-14 19:43:04 +0200

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import ToastComponent from "@/components/ToastComponent";
 
