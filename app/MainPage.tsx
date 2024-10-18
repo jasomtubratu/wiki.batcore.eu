9ecb60e113d20d3736b593e31020502920e3bd88
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2024-10-18 21:47:07 +0200

    Optimized code into layout.tsx (design)

--- app/MainPage.tsx
+++ app/MainPage.tsx
@@ -58,10 +58,6 @@ export default function Home({
     <>
       <Navbar />
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div aria-hidden="true" className="absolute inset-0 h-max w-full m-auto grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
-          <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700" />
-          <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600" />
-        </div>
 
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>Znalostná Databáza&nbsp;</h1>
