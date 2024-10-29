58cd6f40e56a153e63d5fcf9818a565c7a2c96d1
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2024-10-29 17:50:54 +0100

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -31,17 +31,17 @@ export default function Error({ error }: ErrorProps) {
   }, [errorInfo]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 text-center">
         <div>
           <IconAlertTriangle className="mx-auto h-12 w-12 text-yellow-400" />
-          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Ups..</h2>
-          <p className="mt-2 text-sm text-gray-600">
+          <h2 className="mt-6 text-3xl font-extrabold">Ups..</h2>
+          <p className="mt-2 text-sm">
             Niečo sa pokazilo pri načítaní stránky.
           </p>
         </div>
         <div className="mt-8 space-y-6">
-          <p className="text-md text-gray-500">
+          <p className="text-md">
             Veľmi nás to mrzí. Kontaktujte nás ak sa problém bude opakovať.
           </p>
           <div className="flex justify-center">
