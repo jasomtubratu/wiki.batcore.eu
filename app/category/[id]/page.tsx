778c07c77531780cefd05186c130f96edb9c8504
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2024-10-31 11:23:04 +0100

    Forced category to be dynamic

--- app/category/[id]/page.tsx
+++ app/category/[id]/page.tsx
@@ -4,6 +4,8 @@ import CategoryComponent from "./CategoryComponent";
 
 import prisma from "@/prisma/client";
 
+export const dynamic = "force-dynamic";
+
 interface Article {
     id: string;
     title: string;
