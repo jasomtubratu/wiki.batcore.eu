a5cf64a0100167481d3d8df25ffa9e851191903b
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2024-10-31 11:22:55 +0100

    Fixed cloudlfare IP address

--- utils/handleIP.ts
+++ utils/handleIP.ts
@@ -4,11 +4,16 @@ function isValidIPAddress(ip: string): boolean {
   const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   const ipv6Regex = /^(([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4}|:)|(([0-9a-fA-F]{1,4}:){1,6}|:):(([0-9a-fA-F]{1,4}:){1,5}|:):(([0-9a-fA-F]{1,4}:){1,4}|:):(([0-9a-fA-F]{1,4}:){1,3}|:):(([0-9a-fA-F]{1,4}:){1,2}|:):(([0-9a-fA-F]{1,4}:)|:):([0-9a-fA-F]{1,4}|:)|(([0-9a-fA-F]{1,4}:){1,7}|:):|:((:[0-9a-fA-F]{1,4}){1,7}|:))$/;
 
-  return ipv4Regex.test(ip) || ipv6Regex.test(ip);
+  const isValid = ipv4Regex.test(ip) || ipv6Regex.test(ip);
+  return isValid;
 }
 
 export function encryptIPAddress(ip: string): string | null {
-  if (!isValidIPAddress(ip)) {
+  const ips = ip.split(",").map((ip) => ip.trim());
+
+  const validIP = ips.find(isValidIPAddress);
+
+  if (!validIP) {
     return null;
   }
 
@@ -18,10 +23,10 @@ export function encryptIPAddress(ip: string): string | null {
     throw new Error('SALT_KEY environment variable is not set');
   }
 
-  const saltedIP = ip + saltKey;
+  const saltedIP = validIP + saltKey;
+
   const cipher = crypto.createCipher('aes-256-cbc', saltKey);
   let encrypted = cipher.update(saltedIP, 'utf8', 'hex');
-
   encrypted += cipher.final('hex');
 
   return encrypted;
