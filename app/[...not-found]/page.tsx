80afecbd51d9cbee953d8c09a56864a56d48b4d5
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2024-11-01 13:47:28 +0100

    Added bacgrkound for 404

--- app/[...not-found]/page.tsx
+++ app/[...not-found]/page.tsx
@@ -8,6 +8,13 @@ import Link from "next/link";
 export default function Error404() {
   return (
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div
+        aria-hidden="true"
+        className="absolute inset-0 h-max w-full m-auto grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20"
+      >
+        <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700" />
+        <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600" />
+      </div>
       <div className="max-w-md w-full space-y-8 text-center">
         <div>
           <IconAlertTriangle className="mx-auto h-12 w-12 text-yellow-400" />
--- utils/handleIP.ts
+++ utils/handleIP.ts
@@ -5,6 +5,7 @@ function isValidIPAddress(ip: string): boolean {
   const ipv6Regex = /^(([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4}|:)|(([0-9a-fA-F]{1,4}:){1,6}|:):(([0-9a-fA-F]{1,4}:){1,5}|:):(([0-9a-fA-F]{1,4}:){1,4}|:):(([0-9a-fA-F]{1,4}:){1,3}|:):(([0-9a-fA-F]{1,4}:){1,2}|:):(([0-9a-fA-F]{1,4}:)|:):([0-9a-fA-F]{1,4}|:)|(([0-9a-fA-F]{1,4}:){1,7}|:):|:((:[0-9a-fA-F]{1,4}){1,7}|:))$/;
 
   const isValid = ipv4Regex.test(ip) || ipv6Regex.test(ip);
+
   return isValid;
 }
 
@@ -27,6 +28,7 @@ export function encryptIPAddress(ip: string): string | null {
 
   const cipher = crypto.createCipher('aes-256-cbc', saltKey);
   let encrypted = cipher.update(saltedIP, 'utf8', 'hex');
+
   encrypted += cipher.final('hex');
 
   return encrypted;
