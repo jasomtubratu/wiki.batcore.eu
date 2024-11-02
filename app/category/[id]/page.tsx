f7a4210c507bd85e2c3001b94c7e8c705ef9ba49
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2024-11-02 19:41:07 +0100

    Fixed category count by me

--- app/category/[id]/page.tsx
+++ app/category/[id]/page.tsx
@@ -7,66 +7,70 @@ import prisma from "@/prisma/client";
 export const dynamic = "force-dynamic";
 
 interface Article {
-    id: string;
-    title: string;
-    updatedAt: Date;
-    emoji: string;
+  id: string;
+  title: string;
+  updatedAt: Date;
+  emoji: string;
+  count: number;
 }
 
-export default async function Article({
-    params,
-}: {
-    params: { id : string };
-}) {
+export default async function Article({ params }: { params: { id: string } }) {
+  if (!params.id) notFound();
+  if (
+    params.id !== "vps" &&
+    params.id !== "minecraft" &&
+    params.id !== "ostatne"
+  )
+    notFound();
 
-    if (!params.id) notFound();
-    if (params.id !== "vps" && params.id !== "minecraft" && params.id !== "ostatne") notFound();
-    
-    const thatArticle = await prisma.article.findMany({
-        where: {
-            category: params.id,
-            isDeleted: false,
-            isPublic: true,
-        },
-        select: {
-            id: true,
-            title: true,
-            emoji: true,
-            updatedAt: true,
-        },
-    });
+  const thatArticle = await prisma.article.findMany({
+    where: {
+      category: params.id,
+      isDeleted: false,
+      isPublic: true,
+    },
+    select: {
+      id: true,
+      title: true,
+      emoji: true,
+      updatedAt: true,
+    },
+  });
 
-    if (!thatArticle) notFound();
+  if (!thatArticle) notFound();
 
-    const articleViews = await prisma.articleView.groupBy({
-        by: ['articleId'],
-        _count: {
-            articleId: true,
-        },
-        where: {
-            articleId: {
-                in: thatArticle.map(article => article.id),
-            },
-        },
-    });
+  const articleViews = await prisma.articleView.findMany({
+    where: {
+      articleId: {
+        in: thatArticle.map((article) => article.id),
+      },
+    },
+    select: {
+      articleId: true,
+      count: true,
+    },
+  });
 
-    const viewCounts: Record<string, number> = articleViews.reduce((acc: Record<string, number>, view) => {
-        acc[view.articleId] = view._count.articleId;
+  const viewCounts: Record<string, number> = articleViews.reduce(
+    (acc: Record<string, number>, view) => {
+      acc[view.articleId] = view.count;
 
-        return acc;
-    }, {});
+      return acc;
+    },
+    {}
+  );
 
-    const formattedArticles = thatArticle.map((article) => {
-        return {
-            id: article.id,
-            title: article.title,
-            updatedAt: article.updatedAt,
-            emoji: article.emoji,
-            count: viewCounts[article.id] || 0,
-        };
-    });
+  const formattedArticles = thatArticle.map((article) => {
+    return {
+      id: article.id,
+      title: article.title,
+      updatedAt: article.updatedAt,
+      emoji: article.emoji,
+      count: viewCounts[article.id] || 0,
+    };
+  });
 
-
-    return <CategoryComponent articles={formattedArticles} category={params.id} />;
-
-};
+  return (
+    <CategoryComponent articles={formattedArticles} category={params.id} />
+  );
+}
